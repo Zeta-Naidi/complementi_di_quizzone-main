85a989f66ce246ad0e841d34f16058fbbf265e34
Zeta-Naidi <znaidi2003@gmail.com>
2025-06-29 13:26:22 +0200

    Fix port for Render deployment

--- complementi_quiz.py
+++ complementi_quiz.py
@@ -65,4 +65,7 @@ def reset():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(port=777, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
